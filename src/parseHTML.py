3bbcd2f4eb323494c32e0c7de6f97af5a87253b5
Argo Kamenik <argo.kamenik@gmail.com>
2022-10-03 11:14:54 +0300

    Started parsing HTML

--- src/parseHTML.py
+++ src/parseHTML.py
@@ -1,11 +1,35 @@
-# Classes for parsing Stuudium grades page HTML
+import html.parser
+
+gradesTable = False
+grades = {}
+
+class GradePageParser(html.parser.HTMLParser):
+
+    def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]) -> None:
+        global gradesTable
+        if (len(attrs) > 0):
+            if (attrs[0][1] == "student_lessons_grades  not_separate_summary_lessons"):
+                gradesTable = True
+    
+    def handle_endtag(self, tag: str) -> None:
+        global gradesTable
+        if (tag == "table"):
+            gradesTable = False
+
+    def handle_data(self, data: str) -> None:
+        # print("Data: " + data)
+        if gradesTable and data.rstrip() != "":
+            print("Data: " + data)
 
 
-import http
-import requests
+file = open("grades.html", encoding="UTF-8")
+rawHTML = ""
 
-userName = "Sass Kristofer Kase"
-passwd = "seeonproov1"
+for line in file:
+    rawHTML += line
+file.close()
 
 
+parser = GradePageParser()
 
+parser.feed(rawHTML)
