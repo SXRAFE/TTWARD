868801537f1b141c4418d514189b9965387f8b3f
Argo Kamenik <argo.kamenik@gmail.com>
2022-10-17 11:12:42 +0300

    Grades table parsing now functional (somewhat)

--- src/parseHTML.py
+++ src/parseHTML.py
@@ -1,25 +1,67 @@
 import html.parser
 
 gradesTable = False
-grades = {list()}
+gradesRowHeader = False
+gradeContainer = False
+grades = {}
+currentSubject = []
+currentSubjectName = str()
 
 class GradePageParser(html.parser.HTMLParser):
 
     def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]) -> None:
         global gradesTable
-        if (len(attrs) > 0):
-            if (attrs[0][1] == "student_lessons_grades  not_separate_summary_lessons"):
-                gradesTable = True
+        global grades
+        global currentSubject
+        global currentSubjectName
+        global gradesRowHeader
+        global gradeContainer
+
+        if (tag == "tbody"):
+            gradesTable = True
+        if (gradesTable):
+            if (tag == "tr"):
+                currentSubject = []
+                currentSubjectName = ""
+            elif (tag == "th"):
+                gradesRowHeader = True
+            elif (len(attrs) > 0):
+                if attrs[0][1] == "grade_container":
+                    gradeContainer = True
+
+
     
     def handle_endtag(self, tag: str) -> None:
         global gradesTable
+        global currentSubject
+        global currentSubjectName
         if (tag == "table"):
             gradesTable = False
+        if (tag == "tr"):
+            currentSubject = []
+            currentSubjectName = ""
 
     def handle_data(self, data: str) -> None:
+        global grades
+        global gradesTable
+        global currentSubject
+        global currentSubjectName
+        global gradesRowHeader
+        global gradeContainer
         # print("Data: " + data)
-        if gradesTable and data.rstrip() != "":
-            print("Data: " + data)
+        if gradesTable:
+            if data.rstrip() != "":
+                if currentSubject == [] and currentSubjectName == "":
+                    currentSubjectName = data
+                if gradesRowHeader:
+                    if not data in grades.keys():
+                        grades[data] = []
+                    gradesRowHeader = False
+                if gradeContainer:
+                    grades[currentSubjectName].append(data)
+                    gradeContainer = False
+                print("Data: " + data)
+        
 
 
 file = open("grades.html", encoding="UTF-8")
@@ -32,3 +74,5 @@ file.close()
 parser = GradePageParser()
 
 parser.feed(rawHTML)
+
+print(grades)
